461a90c74425dd17d1b4a856dc5cd2f4982ba89f
Larissa Cristina <larissacristina.nunes@outlook.com>
2021-11-24 13:23:56 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 let tarefasDigitadas = document.getElementById('texto-tarefa');
 let listaTarefas = document.getElementById('lista-tarefas');
+let botaoTarefas = document.getElementById('criar-tarefa');
+
+function adicionandoTarefas() {
+  const tarefa = document.createElement('li');
+  tarefa.className = 'tarefa';
+  const texto = tarefasDigitadas.value;
+  tarefa.innerText = texto;
+  listaTarefas.appendChild(tarefa);
+  tarefasDigitadas.value = '';
+}
+
+botaoTarefas.addEventListener('click', adicionandoTarefas);
+
+window.onload = function init() {
+};
